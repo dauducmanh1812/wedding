4af88eeff3f5c395a0ad874127f43276496e457d
Đỗ Đức Mạnh <dauducmanh.1812>
2025-09-23 01:06:36 +0700

    Update

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -3,11 +3,15 @@ import "../assets/styles/01-hero.scss";
 function SectionHero({ siteData }) {
   return (
     <div className="section section-hero">
-      <div className="element-transtion arcittya-begatri">
-        <h1>{siteData.couple.shortName_bride.toUpperCase()}</h1>
-        <div className="section-hero_groom">
+      <div className="arcittya-begatri">
+        <h1 className="fist-name">
+          {siteData.couple.shortName_bride.toUpperCase()}
+        </h1>
+        <div className="second">
           <span>&</span>{" "}
-          <h1>{siteData.couple.shortName_groom.toUpperCase()}</h1>
+          <h1 className="second-name">
+            {siteData.couple.shortName_groom.toUpperCase()}
+          </h1>
         </div>
       </div>
     </div>
