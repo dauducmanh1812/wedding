913c8c5ac61f891027068f26e443a2346bc6b361
Đỗ Đức Mạnh <dauducmanh.1812>
2025-09-27 21:54:36 +0700

    Update

--- src/components/SectionSaveTheDate.js
+++ src/components/SectionSaveTheDate.js
@@ -5,6 +5,7 @@ import couple1 from "../assets/images/02-save-the-date/couple-1-optimized.webp";
 
 function SectionSaveTheDate({ siteData }) {
   const animationRef = useScrollAnimation();
+  const imageRef = useScrollAnimation();
 
   return (
     <div className="section section-save-the-date">
@@ -16,7 +17,7 @@ function SectionSaveTheDate({ siteData }) {
         <h1 className="save-the-date monsieur-la-doulaise-regular">Save the date</h1>
         <h1 className="date">{siteData.event.date}</h1>
       </section>
-      <img src={couple1} alt="Couple" />
+      <img ref={imageRef} className="flip-in-y" src={couple1} alt="Couple" />
     </div>
   );
 }
