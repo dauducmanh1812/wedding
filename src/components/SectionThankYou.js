b0d26a576edbaa9b90895bd2ee63a2ae29f7d9b0
Đỗ Đức Mạnh <dauducmanh.1812>
2025-09-27 20:28:50 +0700

    Update

--- src/components/SectionThankYou.js
+++ src/components/SectionThankYou.js
@@ -3,7 +3,7 @@ import "../assets/styles/08-thank-you.scss";
 function SectionThankYou({ siteData }) {
   return (
     <div className="section section-thank-you">
-      <div className="thank-you-content eb-garamond-regular">{siteData.thankYou.message}</div>
+      <div className="thank-you-content">{siteData.thankYou.message}</div>
       <div className="thank-you-content monsieur-la-doulaise-regular signature">
         {siteData.thankYou.signature}
       </div>
